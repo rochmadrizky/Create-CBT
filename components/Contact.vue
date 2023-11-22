a0ef6fc50bbe7e31b1b9583f3a0ab0c9af54023d
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2023-11-22 18:53:51 +0700

    login dan informasi kontak

---
+++ components/Contact.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-200">
+    <div class="bg-white p-8 rounded shadow-md w-96">
+      <h2 class="text-2xl font-semibold mb-4 text-center">Informasi Kontak</h2>
+
+      <div class="mb-4">
+        <label for="nama" class="block text-gray-700 text-sm font-bold mb-2"
+          >Nama</label
+        >
+        <input
+          v-model="nama"
+          type="text"
+          id="nama"
+          class="w-full p-2 border rounded"
+          placeholder="Masukkan nama"
+        />
+      </div>
+
+      <div class="mb-4">
+        <label for="email" class="block text-gray-700 text-sm font-bold mb-2"
+          >Email</label
+        >
+        <input
+          v-model="email"
+          type="email"
+          id="email"
+          class="w-full p-2 border rounded"
+          placeholder="Masukkan email"
+        />
+      </div>
+
+      <div class="mb-4">
+        <label for="telpon" class="block text-gray-700 text-sm font-bold mb-2"
+          >Nomor Telepon</label
+        >
+        <div class="flex">
+          <select
+            v-model="pilihanNegara"
+            class="w-10 p-2 border rounded appearance-none"
+          >
+            <option value="us">US</option>
+            <option value="sa">SA</option>
+            <option value="id">ID</option>
+          </select>
+
+          <input
+            v-model="telpon"
+            type="telpon"
+            id="telpon"
+            class="w-72 p-2 border rounded"
+            :placeholder="`${kodeNegara[pilihanNegara]}`"
+            @input="validasiNomer"
+          />
+        </div>
+      </div>
+
+      <div class="mb-4">
+        <label
+          for="description"
+          class="block text-gray-700 text-sm font-bold mb-2"
+          >Deskripsi</label
+        >
+        <textarea
+          v-model="deskripsi"
+          id="deskripsi"
+          class="w-full p-2 border rounded"
+          placeholder="Masukkan deskripsi"
+        ></textarea>
+      </div>
+
+      <button
+        @click="tombolKirim"
+        class="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+      >
+        Kirim
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const nama = ref("");
+const email = ref("");
+const pilihanNegara = ref("id");
+const telpon = ref("");
+const deskripsi = ref("");
+
+const kodeNegara = {
+  us: "+1",
+  sa: "+966",
+  id: "+62",
+};
+
+const validasiNomer = () => {
+  telpon.value = telpon.value.replace(/\D/g, "");
+};
+
+const tombolKirim = () => {
+  console.log("Formulir dikirim");
+};
+</script>
---
+++ components/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-200">
+    <div class="bg-white p-8 rounded shadow-md w-96">
+      <h2 class="text-2xl font-semibold mb-4">Login CBT</h2>
+
+      <div class="mb-4">
+        <label for="email" class="block text-gray-700 text-sm font-bold mb-2"
+          >Email</label
+        >
+        <input
+          v-model="email"
+          type="text"
+          id="email"
+          class="w-full p-2 border rounded"
+          placeholder="Masukan email dulu"
+        />
+      </div>
+
+      <div class="mb-4">
+        <label for="password" class="block text-gray-700 text-sm font-bold mb-2"
+          >Password</label
+        >
+        <div class="relative">
+          <input
+            v-model="password"
+            :type="tampilkanPassword ? 'text' : 'password'"
+            id="password"
+            class="w-full p-2 border rounded"
+            :placeholder="tampilkanPassword ? 'Masukan password' : '*********'"
+          />
+          <button
+            @click="tombolTampilkanPassword"
+            class="absolute top-0 right-0 mt-2 mr-3 text-sm text-blue-500 cursor-pointer focus:outline-none"
+          >
+            {{ tampilkanPassword ? "Sembunyikan" : "Tampilkan" }}
+          </button>
+        </div>
+      </div>
+
+      <button
+        @click="login"
+        class="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+      >
+        Login
+      </button>
+
+      <p class="mt-4 text-blue-500">
+        <span class="font-semibold text-black">Note:</span>
+        Silahkan login dahulu gaes untuk memulai ujian, Semoga sukses dan diberi
+        kelancaran yaa:)
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const email = ref("");
+const password = ref("");
+const tampilkanPassword = ref(false);
+
+const tombolTampilkanPassword = () => {
+  tampilkanPassword.value = !tampilkanPassword.value;
+};
+
+const login = () => {
+  console.log("coba tombol");
+};
+</script>
